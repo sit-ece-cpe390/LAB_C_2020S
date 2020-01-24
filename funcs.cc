de6391a1dab39022d17c056f4fa11020b981a3aa
Veggietay97 <dtay@stevens.edu>
2020-01-24 15:33:11 -0500

    change checkprime

--- funcs.cc
+++ funcs.cc
@@ -29,9 +29,9 @@ uint32_t sumsq(uint32_t a, uint32_t b);
 uint32_t countPrimes(uint32_t a, uint32_t b);
 bool isPrime(uint32_t p)
 {
+  uint32_t p = 0;
   cout << "Input a number";
   cin >> p;
-  uint32_t i = 0;
   int truefalse = 0;
     for(i = 2; i<=p/2;i++)
       {

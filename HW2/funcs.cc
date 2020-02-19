a14e417f15d1a2ccd76a98bc50f639b80e51bb8c
HanyiZhang <31740048+HanyiZhang@users.noreply.github.com>
2020-02-19 13:49:56 -0500

    123

---
+++ HW2/funcs.cc
@@ -0,0 +1,12 @@
+/*
+ Write your code here for the required functions
+ The makefile builds the code, assuming the functions here match the ones used in main
+*/
+
+#include <cmath>
+double hypotenuse(double a, double b){
+    return sqrt(a*a + b*b);
+}
+double mean(double a, double b){
+    return (a+b)/2;
+}
